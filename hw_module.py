ab2cd02773f02e8746e440e2db99be74799fef0f
David <David@Biancas-MacBook-Pro.local>
2022-12-22 13:46:04 -0600

    first commit

---
+++ hw_module.py
@@ -0,0 +1,12 @@
+import math
+
+def squareFootage(length, width):
+    print(f"This house has {length*width} sq.ft")
+
+def circumference(radius):
+    print(f"This circle has a circumference of {2*math.pi*radius}")
+
+
+
+
+
